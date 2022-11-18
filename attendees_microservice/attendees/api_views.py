550633e881a257c77ff60cfd90df93ecb8ab20be
Steven <Stevenhuytran03@gmail.com>
2022-11-18 00:42:53 -0800

    Updated show attendee requests

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -98,29 +98,36 @@ def api_list_attendees(request, conference_vo_id=None):
         )
 
 
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_attendee(request, pk):
-    """
-    Returns the details for the Attendee model specified
-    by the pk parameter.
+    if request.method == "GET":
+        attendee = Attendee.objects.get(id=pk)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
 
-    This should return a dictionary with email, name,
-    company name, created, and conference properties for
-    the specified Attendee instance.
+    elif request.method == "DELETE":
+        count, _ = Attendee.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
 
-    {
-        "email": the attendee's email,
-        "name": the attendee's name,
-        "company_name": the attendee's company's name,
-        "created": the date/time when the record was created,
-        "conference": {
-            "name": the name of the conference,
-            "href": the URL to the conference,
-        }
-    }
-    """
-    attendee = Attendee.objects.get(id=pk)
-    return JsonResponse(
-        attendee,
-        encoder=AttendeeDetailEncoder,
-        safe=False,
-    )
+    else:
+        content = json.loads(request.body)
+
+        try:
+            if "conference" in content:
+                conference = ConferenceVO.objects.get(id=content["conference"])
+                content["conference"] = conference
+        except ConferenceVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid conference id"}, status=400
+            )
+
+        Attendee.objects.filter(id=id).update(**content)
+        attendee = Attendee.objects.get(id=pk)
+        return JsonResponse(
+            attendee,
+            encoder=AttendeeDetailEncoder,
+            safe=False,
+        )
